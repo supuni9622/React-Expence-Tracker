ab9f74502eb00e4e36a3299150df6499d01f9df3
Supuni22 <supunimanamperi@gmail.com>
2020-11-08 11:17:01 +0530

    Created incomeExpense functions

--- expence-tracker/src/components/Header.js
+++ expence-tracker/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <h1 className='p-2 m-3' style={{textAlign:'center'}}>
-            Expense Tracker Header
+            Expense Tracker 
         </h1>
     )
 }
--- expence-tracker/src/components/IncomeExpenses.js
+++ expence-tracker/src/components/IncomeExpenses.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Card} from 'react-bootstrap'
+import {GlobalContext} from '../context/GlobalState'
 
 const IncomeExpenses = () => {
+
+    const {transactions} = useContext(GlobalContext)
+
+    const amounts = transactions.map(transaction => transaction.amount);
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item),0)
+        .toFixed(2)
+    
+    const expense = (amounts.filter(item => item < 0).reduce((acc, item)=> (acc += item),0)* -1)
+                    .toFixed(2)
+    
     return (
         <div className='d-flex justify-content-center'>
             <Card className='px-5 py-2'>
                 <h4> Income </h4>
-                <p id="money-plus" className='money-plus'> + $0.00 </p>
+                <p id="money-plus" className='money-plus'> + ${income} </p>
             </Card>
             <Card className='px-5 py-2'>
                 <h4> Expense </h4>
-                <p id="money-minus" className='money-minus'> - $0.00 </p>
+                <p id="money-minus" className='money-minus'> - ${expense} </p>
             </Card>   
         </div>
     )
